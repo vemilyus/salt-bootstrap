bfc3567784ff068338939a96fbe3f0bb458052f3
Alex Katlein <dev@vemilyus.com>
2022-03-27 23:42:23 +0200

    Created bootstrap bootstrapping script

---
+++ bootstrap.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+set -e +x # Exit after failed command but don't print commands
+
+#################
+### FUNCTIONS ###
+#################
+
+function err_exit() {
+  echo >&2 $1
+  echo >&2 ""
+  exit 1
+}
+
+function check_binary() {
+  printf "Checking binary %s " $1
+
+  if ! which "$1" >/dev/null 2>&1; then
+    err_exit "[ NOT FOUND ]"
+  fi
+
+  echo "[ OK ]"
+}
+
+function reset_dir() {
+  echo "Leaving bootstrap directory"
+  cd $1
+}
+
+######################
+### ARG VALIDATION ###
+######################
+case "$1" in
+"master" | "minion")
+  SCRIPT_DIR=$1
+  ;;
+"")
+  err_exit 'Expected argument: "master" or "minion"'
+  ;;
+*)
+  err_exit "Unrecognized argument \"$1\""
+  ;;
+esac
+
+#####################
+### PREREQUISITES ###
+#####################
+
+echo "Checking out bootstrap repo"
+echo ""
+
+echo "Checking prerequisites:"
+
+check_binary "git"
+
+echo ""
+
+#########################
+### CHECKING OUT REPO ###
+#########################
+
+mkdir -p "${HOME}/git"
+
+LOCAL_PATH="${HOME}/git/salt-bootstrap"
+
+if [ ! -d "${LOCAL_PATH}" ]; then
+  echo "Checking out bootstrap repo"
+  git clone "https://github.com/vemilyus/salt-bootstrap.git" "${LOCAL_PATH}"
+
+  echo ""
+fi
+
+####################
+### PREPARATIONS ###
+####################
+
+# We always want to return to the original directory, it's just cleaner that way
+PREV_DIR=$(pwd)
+trap 'reset_dir $PREV_DIR' EXIT
+
+echo "Entering bootstrap directory (${LOCAL_PATH})"
+cd "${LOCAL_PATH}"
+echo ""
+
+echo "Updating bootstrap repository"
+git pull >/dev/null
+
+echo ""
+
+######################
+### DOING THE WORK ###
+######################
+
+set +e # We're fine with this failing
+chmod +x **/*.sh >/dev/null 2>&1
+set -e
+
+./${SCRIPT_DIR}/bootstrap.sh
+
+###############
+### CLEANUP ###
+###############
+
+echo ""
+echo "Done bootstrapping"
